5f5c1e9986276cf6ab453a42755bc887712c6745
Ash K <krishan.ash:gmail.com>
2015-02-21 23:34:40 +0000

    Refactored copy function

--- copy_test.py
+++ copy_test.py
@@ -1,56 +1,47 @@
-
 import os
 import fnmatch
 import shutil
 
-def copy_files():
-    pattern = 'outlook*.txt'
-    for root,dirs,files in os.walk('c:\\release'):
-         #print root
-        for f in files:                
-                
-                #print (os.listdir())
-                #dirlist.append(os.listdir(root))
-                if len(fnmatch.filter(files,pattern)) > 0:
-                    testpath = (os.path.join(root,f).split('\\'))[:-1]
-                    releasepath = (root)
-                    #tdir = os.path.dirname(os.path.join(root,f))
-                    #print root
-                    
-                    #print "this is dir %s" % tdir
-                    #relitem = testpath[-2]
-                    #relfullpath = (root)
-                    #print f
-                    break
-                    #reloutlookdir = (test[-2])
-    #print testpath
-    #print reldir                #prinst reloutlookdir
-    #print root
-    for root,dirs,files in os.walk('c:\\LiberateDev'):
-         #print root
-        for f in files:                
-                
-                #print (os.listdir())
-                #dirlist.append(os.listdir(root))
-                if len(fnmatch.filter(files,pattern)) > 0:
-                    devpath =  (os.path.join(root,f).split('\\'))[:-1]
-                    devdir = devpath[-1]
-                    devfullpath = (root)
-                    break
-                    #reloutlookdir = (test[-2])
+def get_paths_based_on_pattern(abspathname,pattern):
+    for root,dirs,files in os.walk(abspathname,topdown=False):
+            for f in files:
+                for filename in fnmatch.filter(files,pattern):
+                    if len(fnmatch.filter(files,pattern)) > 0:                        
+                        splitpath = (os.path.join(root,filename).split('\\'))[:-1]
+                        print splitpath
+                        rootpath = (root)
+                        foldername = splitpath[-1]
+                        break
+    return splitpath, rootpath, foldername
+       
+def main():    
+    outlook_pattern = 'outlook*'
+    excel_pattern = 'excel*'
+    release_full_path = r'c:\release'
+    dev_full_path = r'c:\LiberateDev'
+    releasepath,releaseroot,releasefoldername = get_paths_based_on_pattern(release_full_path,outlook_pattern)
+    devpath, devroot,devdir = get_paths_based_on_pattern(dev_full_path,outlook_pattern)
+    match_and_rename_release(releasepath,releaseroot,devdir)
         
-    #print devdir                #prinst reloutlookdir
-    print devfullpath
-    print releasepath
-    testpath[-1] = devdir
-    #testpath[-2] = reldir
-    print devpath
-    newpath = testpath[0] + os.sep + os.path.relpath(os.path.join(*testpath))
-    print newpath
+def match_and_rename_release(releasepath,releaseroot,sourcedir):
+    releasepath[-1] = sourcedir #replace directory name from development in release folder
+    newpath = releasepath[0] + os.sep + os.path.relpath(os.path.join(*releasepath))
     if not os.path.isdir(newpath):
-        shutil.copytree(releasepath,newpath)
-        shutil.rmtree(releasepath)
-    else:
-        print "directory already exists"
+        shutil.copytree(releaseroot,newpath)
+        shutil.rmtree(releaseroot)
+    elif os.path.isdir(newpath):
+        print ("Directory already exists,copying correct addin files in this %s directory from %s" % (newpath,releaseroot))
+        copytree(releaseroot,newpath)
+
+#helper function
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+        
 if __name__ == "__main__":
-    copy_files()
+    main()
