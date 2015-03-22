61e0872e71ac6a022df79aa0e6767d20b534f72d
Ash K <krishan.ash:gmail.com>
2015-03-22 12:42:27 +0000

    Refactored new-compare and fixed comparison for versions

--- new-compare.py
+++ new-compare.py
@@ -1,11 +1,18 @@
 import os
 from win32api import GetFileVersionInfo, LOWORD, HIWORD  #Referred to VB API from MSDN to retrieve version attributes for dll and exe files 
 #import csv
+from openpyxl import Workbook
 
 sourceDir = r'c:\NewRelease'
-targetDir = r'c:\LiberateDev\AutoUpdates'
+targetDir = r'c:\release'
+#targetDir = r'c:\LiberateDev\AutoUpdates'
 targetDir2 = r'c:\LiberateDev'
 dst_workbook = r'c:\test\output\out.xlsx'
+wb = Workbook()
+ws1 = wb.active
+ws1.title = "Files common and version"
+ws1.append(("Filename","New Release Version","Current Liberate Version","File Extension"))
+
 
 def main():
     print "test"
@@ -16,66 +23,32 @@ def relative_files(path):
     """Generate filenames with pathnames relative to the initial path."""
     for root, dirnames, files in os.walk(path):
         relroot = os.path.relpath(root, path)
-        #print relroot
         for filename in files:
-            fullpath = os.path.join(root,filename)
-            #print filename
-            yield os.path.join(relroot, filename), fullpath
-  
-def target_relative_files(path):
-    """Generate filenames with pathnames relative to the initial path."""
-    for root, dirnames, files in os.walk(path):
-        relroot = os.path.relpath(root, path)
-        #print relroot
-        for filename in files:
-            fullpath = os.path.join(root,filename)
-            #print filename
-            yield os.path.join(relroot, filename), fullpath     
-            
-            
-            
-def compare_files():
-    
-    
-    #for f in relative_files(r'c:\release'):
-        #print f
-    set1 = set(relative_files(sourceDir))
-    file1 = set(x[0] for x in set1)
-    file1fullpath = ([x[1] for x in set1])
-    
-    #print file1
-    #print file1fullpath[0]
-    
-    set2 = set(relative_files(targetDir))
-    file2 = set(x[0] for x in set2)
-    file2fullpath = [x[1] for x in set2]
-    
-
-    #print file2
-    #print file2fullpath
-    #print file1path
-    for same in file1.intersection(set(x[0] for x in target_relative_files(targetDir))):
-        #print same
-        srcVersion = print_version(same)
-        #for f_in_target in file2.intersection(set):
-            
+            yield os.path.join(relroot, filename)#fullpath
         
-        for same2 in file2.intersection(file1):
-            #if same == same2:
-                #targetVersion = print_version(file2fullpath[0])
-        #dstVersion = print
-                print "%s,%s" % (same, srcVersion)
+         
+def compare_files():          #File comparison function, it calls generator relative_files to  iterate over files found after converting it to sets 
+    file1 = set(relative_files(sourceDir))
+    for same in file1.intersection(set(relative_files(targetDir))):    #Compare using set operation intersaction and only perform function on matched files
+        srcfullpath = os.path.join(sourceDir,same)
+        trgfullpath = os.path.join(targetDir,same)
+        if os.path.isfile(srcfullpath) and os.path.isfile(trgfullpath):
+            srcVersion = print_version(srcfullpath)
+            trgVersion = print_version(trgfullpath)
+            
+            fname,file_extension = os.path.splitext(srcfullpath)   #Get file extensions from filename
+            file_extension = file_extension.replace(".","")   #example: Replace .txt to txt
+            
+            ws1.append((same,srcVersion,trgVersion, file_extension))
+    wb.save(filename = dst_workbook)
 
-def print_version(sourcePath):
+def print_version(sourcePath):  #returns version number to caller
     if sourcePath.lower().endswith('.exe'):
-                #print "TRUE"
         major,minor,subminor,revision = get_version_info(sourcePath)
         ver = ("%s.%s.%s.%s" % (major,minor,subminor,revision))
-        #print ver
         return ver
     return         
     
-
 
 def get_version_info(filename):
     try:
